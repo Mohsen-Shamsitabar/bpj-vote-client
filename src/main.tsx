b24e3ae6f736100b588208b06a5223c97aa7e97c
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-12-05 12:04:44 +0330

    refactor & add a theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,38 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "normalize.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("No root element found!");
+
+const theme = createTheme({
+  direction: "rtl",
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#7ba42b",
+    },
+    secondary: {
+      main: "#1f75bb",
+    },
+  },
+  spacing: 16,
+  typography: {
+    fontFamily: "BYekan, sans-serif",
+  },
+});
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );

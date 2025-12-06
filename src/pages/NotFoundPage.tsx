0c9ebc72d5cc13184ed480b2087ab3c67dca8ed6
Shinmo <shamsitabar.mohsen@gmail.com>
2025-12-06 12:08:30 +0330

    refactor `NotFoundPage.tsx` design (#7)

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,10 @@
 const NotFoundPage = () => {
-  return <h1>NotFoundPage</h1>;
+  return (
+    <div className="mt-40 text-center">
+      <h1 className="text-5xl font-bold">404</h1>
+      <h2 className="text-2xl">متأسفانه صفحه مورد نظر یافت نشد.</h2>
+    </div>
+  );
 };
 
 export default NotFoundPage;
